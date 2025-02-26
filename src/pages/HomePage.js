56ce8c58cdc3edc892aa44469810d38749d7e062
ahmed <ahmed.dahdouh98@gmail.com>
2025-02-26 01:23:44 +0100

    Implement filter for sports events based on search input

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,12 +7,19 @@ import BasicCard from "../components/card";
 import {useEffect, useState} from "react";
 import apiService from "../services/AuthService";
 import axios from "axios";
+import InputBase from "@mui/material/InputBase";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function HomePage () {
     const [decoded, setDecoded] = useState(null);
     const [events, setEvents] = useState(
         []
     );
+    const [originalEvents, setOriginalEvents] = useState([]);
 
 
 
@@ -26,10 +33,29 @@ export default function HomePage () {
         axios.get("http://localhost:5000/event/booking")
             .then(response => {
                 setEvents(response.data);
+                setOriginalEvents(response.data);
             }).catch(error => {
             console.log(error);
         })
     }
+
+    const filterEvents = (event) => {
+        const searchTerm = event.target.value.toLowerCase();
+
+        // Filtrer à partir de la liste originale
+        const filtered = originalEvents.filter((event) => {
+            return (
+                event.event_name.toLowerCase().includes(searchTerm) ||
+                event.event_description.toLowerCase().includes(searchTerm) ||
+                event.event_ville.toLowerCase().includes(searchTerm) ||
+                event.event_Items.toLowerCase().includes(searchTerm)
+            );
+        });
+
+        setEvents(filtered);
+    };
+
+
 
     return(
         <div>
@@ -37,7 +63,24 @@ export default function HomePage () {
             <Stack direction="row" spacing={2}
                    justifyContent="center"
                    alignItems="center" paddingTop="20px" >
-                <CustomizedInputBase/>
+                <Paper
+                    component="form"
+                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+                >
+                    <IconButton sx={{ p: '10px' }} aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder="Trouvez Votre plaisir ⛹️🎾⛳"
+                        inputProps={{ 'aria-label': 'Trouvez Votre plaisir' }}
+                        onChange={filterEvents}
+                    />
+                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+
+                </Paper>
                 <TemporaryDrawer/>
             </Stack>
 
