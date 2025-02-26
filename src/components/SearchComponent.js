f54473ccf61cffe263676a198955ead5a06b3a96
ahmed <ahmed.dahdouh98@gmail.com>
2025-02-26 01:23:44 +0100

    Implement filter for sports events based on search input

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -9,22 +9,6 @@ import DirectionsIcon from '@mui/icons-material/Directions';
 
 export default function CustomizedInputBase() {
     return (
-        <Paper
-            component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-        >
-            <IconButton sx={{ p: '10px' }} aria-label="menu">
-                <MenuIcon />
-            </IconButton>
-            <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Trouvez Votre plaisir ⛹️🎾⛳"
-                inputProps={{ 'aria-label': 'Trouvez Votre plaisir' }}
-            />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                <SearchIcon />
-            </IconButton>
-
-        </Paper>
+        <div></div>
     );
 }
